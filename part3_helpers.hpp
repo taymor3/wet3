463694f6fca99f46b9cad5ddfc47b0a3fa6e89fb
Borginer <ariel.itskovich.il@gmail.com>
2025-02-03 16:15:46 +0200

    added given linker and vm
    Signed-off-by: Borginer <ariel.itskovich.il@gmail.com>

--- part3_helpers.hpp
+++ part3_helpers.hpp
@@ -145,6 +145,8 @@ static Buffer* buffer;                          // CodeGen buffer
 static Buffer headerBuffer;                     // HeaderGen buffer
 static map<string, Symbol> symbolTable;			// Table that contains all symbols defined in prog
 static map<string, Function> functionTable;		// Table that contains all functions - each function with it's members
+static int curBlockDepth = 0;
+static int curRetType;
 /**********************************************************************************************/
 
 #endif
