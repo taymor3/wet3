cca205b1646ec0364183c34f53fbcdc721716023
Borginer <ariel.itskovich.il@gmail.com>
2025-02-05 16:13:34 +0200

    final version for now + tests
    Signed-off-by: Borginer <ariel.itskovich.il@gmail.com>

--- part3_helpers.hpp
+++ part3_helpers.hpp
@@ -150,7 +150,7 @@ static int curBlockDepth = 0;
 static int curRetType;
 static int curScopeIntRegsNum = 3; // registers in use of current function
 static int curScopeFloatRegsNum = 3;
-static int curScopeOffset = 0; // Current offset in memory: where to store the next piece of data
+static int curScopeOffset = 0; //offset in memory to store the next data
 static int prevScopeOffset = 0;  
 /**********************************************************************************************/
 
